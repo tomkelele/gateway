bcbab12026f8934a0c53f9f70f8b42370861a40b
Tomkelele <tungxn1594@gmail.com>
2020-10-19 14:24:04 +0700

    initial commit

---
+++ service_1/src/index.php
@@ -0,0 +1,12 @@
+<?php
+
+header('Content-type:application/json;charset=utf-8');
+http_response_code(200);
+$data = [];
+if (array_key_exists('Authorization', getallheaders())) {
+    $data = ["Authorization" => getallheaders()['Authorization']];
+}
+echo json_encode([
+    'code' => 200,
+    'data' => $data,
+]);
